69340d660a84c1d0e91de531a0faa68cd363c48b
apovalixin <apovalixin@bk.ru>
2022-05-23 23:37:05 +0300

    Задача вывода графика синуса

--- Learning/Learning/Learning.cpp
+++ Learning/Learning/Learning.cpp
@@ -2,123 +2,42 @@
 
 using namespace std;
 
-void FillArr(int* const arr, int const size) {
-
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = rand() % 20;
-	}
-}
-
-void ShowArr(int* const arr, int const size) {
-
-	for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl << endl;
-}
-
-void AddBack(int* &arr, int &size, int value) {
-
-	int *NewArr = new int[size + 1];
-
-	for (int i = 0; i < size; i++)
-	{
-		NewArr[i] = arr[i];
-	}
-
-	NewArr[size] = value;
-	
-	size++;
-	
-	delete[] arr;
-	
-	arr=NewArr;
-}
-
-void AddHead(int*& arr, int& size, int value) {
-
-	int* NewArr = new int[size + 1];
-
-	for (int i = 0; i < size; i++)
-	{
-		NewArr[i+1] = arr[i];
-	}
-
-	NewArr[0] = value;
-
-	size++;
-
-	delete[] arr;
-
-	arr=NewArr;
-}
-
-void AddPlace(int*& arr, int& size, int place,  int value) {
-
-	int* NewArr = new int[size + 1];
-
-	for (int i = 0; i < size; i++)
-	{
-		if (i < place)
-		{
-			NewArr[i] = arr[i];
-		}
-
-		if (i>=place)
-		{
-			NewArr[i + 1] = arr[i];
-		}
-
-	}
-
-	NewArr[place] = value;
-
-	size++;
-
-	delete[] arr;
-
-	arr=NewArr;
-}
-
 int main(){
 
 	setlocale(LC_ALL, "ru");
 
-	int size = 0,addback,addhead,addplace,place;
+	char arr[29][59] = {};
 
-	cout << "Введите длину массива: ";
-	cin >> size;
+		for (int i = 0; i < 29; i++)
+		{
+			for (int j = 0; j < 59; j++)
+			{
+				arr[i][j] = ' ';
+			}
+		}
 
-	int* arr = new int[size];
+		for (int i = 0; i < 14; i++)
+		{
+			for (int j = 0; j < 59; j++)
+			{
+				if (sin(j)==i)
+				{
+					arr[i][j] = '*';
+				}
 
-	FillArr(arr, size);
-	ShowArr(arr, size);
+			}
+		}
 
-	cout << "Введите значение, которое нужно добавить в конец массива: ";
-	cin >> addback;
+		for (int i = 0; i < 29; i++)
+		{
+			for (int j = 0; j < 59; j++)
+			{
+				cout << arr[i][j] << " ";
+			}
+			cout << endl;
+		}
 
-	AddBack(arr, size, addback);
-	ShowArr(arr, size);
 
-	cout << "Введите значение, которое нужно добавить в начало массива: ";
-	cin >> addhead;
-
-	AddHead(arr, size, addhead);
-	ShowArr(arr, size);
-
-	cout << "Введите значение, которое нужно добавить в массив: ";
-	cin >> addplace;
-	cout << "Введите порядковый номер элемента, куда добавить элемент: ";
-	cin >> place;
-
-	place--;
-
-	AddPlace(arr, size, place, addplace);
-	ShowArr(arr, size);
-
-	delete[] arr;
 
 	return 0;
 }
