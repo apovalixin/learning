f0bda873f4b9be3d8d9a6eedcb156bbb53efd3df
apovalixin <apovalixin@bk.ru>
2022-06-04 17:00:36 +0300

    Запись точек

---
+++ Learning/Integrator_new/Integrator_new/Integrator_new.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+
+	int size_x = 5, size_y = 5;
+
+	int* x = new int[size_x] {};
+	int* y = new int[size_y]{};
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Введите точку x" << endl;
+		cin >> x[i];
+		cout << "Введите точку y" << endl;
+		cin >> y[i];
+		cout << endl;
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << x[i];
+		cout << y[i] << endl;
+	}
+
+
+
+
+}
+
+
