4997ed36038ab5fc46a1a736a21f0f1e7362b9c2
apovalixin <apovalixin@bk.ru>
2022-06-14 17:54:12 +0300

    Ошибка в алгоритме перемножения матриц

--- Learning/Learning/Learning.cpp
+++ Learning/Learning/Learning.cpp
@@ -3,24 +3,32 @@
 
 using namespace std;
 
-void filling(int **arr,int rows,int cols) {
+void new_matrix(int **arr,int rows,int cols) {
+
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
 
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
 		{
-			arr[i][j] = rand() % 100;
-			if (arr[i][j]<10)
-			{
-				j--;
-			}
-		
+			arr[i][j] = rand() % 10;
 		}
 	}
 }
 
-void showing(int** arr, int rows, int cols) {
+void deleting(int** arr, int rows) {
 
+	for (int i = 0; i < rows; i++)
+	{
+		delete []arr;
+	}
+}
+
+void showing(int **arr, int rows, int cols) {
+	
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
@@ -32,57 +40,65 @@ void showing(int** arr, int rows, int cols) {
 	cout << endl;
 }
 
-void change(int** arr, int rows, int cols) {
-
-	for (int i = 0; i < rows; i++)
-	{
-		int max, x, y;
-		
-		for (int j = 0; j < cols; j++)
-		{
-			if (arr[i][j]>=arr[i][0])
-			{
-				max = arr[i][j];
-				x = i;
-				y = j;
-			}
-		}
-		arr[x][y] = arr[i][0];
-		arr[i][0] = max;
-	}
-
-
-}
-
 int main(){
 
 	setlocale(LC_ALL, "ru");
 	
-	int rows = 0, cols = 0;
-	cout << "Введите число строк "; cin >> rows;
-	cout << "Введите число столбцов "; cin >> cols;
+	int	rows1=0, cols1=0;
+	int rows2=0, cols2=0;
+	int rows3=0, cols3=0;
+
+	cout << "Введите число строк первой матрицы ";	cin >> rows1;
+	cout << "Введите число столбцов первой матрицы ";	cin >> cols1;
 	cout << endl;
 
-	int** arr = new int* [rows];
-	for (int i = 0; i < rows; i++)
+	cols2=rows1;
+	rows2=cols1;
+	rows3 = rows1;
+	cols3 = cols2;
+	
+	int** arr1 = new int* [rows1];
+	new_matrix(arr1, rows1,cols1);
+	showing(arr1, rows1, cols1);
+
+	int** arr2 = new int* [rows2];
+	new_matrix(arr2, rows2, cols2);
+	showing(arr2, rows2, cols2);
+
+	int** arr3 = new int* [rows3];
+	new_matrix(arr3, rows3, cols3);
+
+	int sum=0;
+
+	for (int i = 0; i < rows1; i++)
 	{
-		arr[i] = new int[cols];
+		for (int j = 0; j < cols2+1; j++)
+		{
+				int a=arr1[i][j] * arr2[j][i];
+				int b = b+a;
+				arr3[i][0] = b;
+		}
+		for (int j = 1; j < cols2 + 1; j++)
+		{
+			int a = arr1[i][j-1] * arr2[j][i];
+			int b = b + a;
+			arr3[i][0] = b;
+		}
+
 	}
 
-	filling(arr, rows,cols);
 
-	showing(arr, rows, cols);
+	showing(arr3, rows3, cols3);
 
-	change(arr, rows, cols);
 
-	showing(arr, rows, cols);
+	deleting(arr1, rows1);
+	deleting(arr2, rows2);
+	deleting(arr3, rows3);
 
-	for (int i = 0; i < rows; i++)
-	{
-		delete[]arr[i];
-	}
+	delete[]arr1;
+	delete[]arr2;
+	delete[]arr3;
 
-	delete[] arr;
 
 	return 0;
 }
